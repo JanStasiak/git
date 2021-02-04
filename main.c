c81f3595fecb4b841e37a8473aceb6c695d39208
Jan <janek2001@gmail.com>
2021-02-04 23:38:09 +0100

    01 ZmianametodyPolicz

--- main.c
+++ main.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
-int main() {
+int Policz(int a, int b);
+
+int Policz(int a, int b)
+{
+    return a+b;
+}
+
+int main( )
+{
     printf("Hello, World!\n");
+    printf("W pierwszym branchu; 2+3=", Policz(2,3));
     return 0;
 }
